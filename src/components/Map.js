36048228b4ffd8003e95354928ec8cc0c381cf27
LeeByeongMuk <mansogod@gmail.com>
2021-03-28 18:58:12 +0900

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,9 +3,55 @@ import styled from 'styled-components';
 
 import debounce from '../util/debounce.js';
 
+const Container = styled.section`
+    overflow: hidden;
+    padding: 50px 20px;
+`;
+
 const MapContainer = styled.div`
     width: 100%;
     height: 400px;
+    margin-top: 20px;
+`;
+
+const AddressSection = styled.article`
+    position: relative;
+`;
+
+const AddressInput = styled.input`
+    width: 100%;
+    height: 30px;
+    padding-left: 10px;
+    font-size: 16px;
+    color: #333;
+    box-sizing: border-box;
+`;
+
+const AddressList = styled.ul`
+    display: ${props => props.visibility ? 'block' : 'none'};
+    overflow-y: scroll;
+    position: absolute;
+    top: 29px;
+    left: 0;
+    z-index: 10000;
+    width: 100%;
+    height: 100px;
+    border: 1px solid black;
+    background: #fff;
+    box-sizing: border-box;
+`;
+
+const AddressListItem = styled.li`
+    padding: 5px 0 5px 20px;
+    font-size: 16px;
+    line-height: 20px;
+    cursor: pointer;
+
+    &:hover {
+        text-decoration: underline;
+        background: #2a3b4b;
+        color: #fff;
+    }
 `;
 
 export default class Map extends React.Component {
@@ -16,12 +62,15 @@ export default class Map extends React.Component {
         this.marker = null;
 
         this.state = {
-            addresses: []
+            addresses: [],
+            lat: '37.3595704',
+            lng: '127.105399'
         }
 
         this.addressChange = this.addressChange.bind(this);
         this.getAddresses = this.getAddresses.bind(this);
         this.getAddresses = debounce(this.getAddresses, 1000);
+        this.setMap = this.setMap.bind(this);
     }
 
     loadCDN(callback) {
@@ -29,6 +78,7 @@ export default class Map extends React.Component {
 
         let element = document.createElement('script');
         element.async = true;
+        element.id = 'map-cdn'
         element.src = url;
 
         element.onload = () => {
@@ -36,6 +86,14 @@ export default class Map extends React.Component {
         }
 
         document.body.appendChild(element);
+    }
+
+    removeCDN() {
+        let element = document.getElementById('map-cdn');
+
+        if (element) {
+            element.remove();
+        }
     }
 
     addressChange(event) {
@@ -57,24 +115,36 @@ export default class Map extends React.Component {
         })
         .then(res => res.json())
         .then(res => {
-            console.log(res.addresses);
             this.setState({
                 addresses: res.addresses
             });
         });
     }
 
+    setMap(event) {
+        const target = event.target;
+
+        this.setState({
+            lat: target.dataset.lat,
+            lng: target.dataset.lng,
+            addresses: []
+        }, () => {
+            const latlng = new window.naver.maps.LatLng(this.state.lat, this.state.lng);
+            this.map.setCenter(latlng);
+        });
+    }
+
     initMap() {
         this.loadCDN(() => {
             this.map = new window.naver.maps.Map('map', {
-                center: new window.naver.maps.LatLng(37.3595704, 127.105399),
+                center: new window.naver.maps.LatLng(this.state.lat, this.state.lng),
                 zoom: 17
             });
 
-            this.marker = new window.naver.maps.Marker({
-                position: new window.naver.maps.LatLng(37.3595704, 127.105399),
-                map: this.map
-            });
+            // this.marker = new window.naver.maps.Marker({
+            //     position: new window.naver.maps.LatLng(this.state.lat, this.state.lng),
+            //     map: this.map
+            // });
         });
     }
 
@@ -82,27 +152,34 @@ export default class Map extends React.Component {
         this.initMap();
     }
 
+    componentWillMount() {
+        this.removeCDN();
+    }
+
     render() {
         return (
-            <React.Fragment>
-                <input type="text"
-                    name="address"
-                    value={this.state.address}
-                    onChange={this.addressChange} />
-                <section>
-                    <ul>
+            <Container>
+                <AddressSection>
+                    <AddressInput type="text"
+                        name="address"
+                        value={this.state.address}
+                        onChange={this.addressChange} />
+
+                    <AddressList visibility={this.state.addresses.length > 0 ? 1 : 0}>
                         { this.state.addresses.map((address, index) => {
                             return (
-                                <li key={index}>{ address.jibunAddress }</li>
-                            );
+                                <AddressListItem key={index}
+                                    data-lat={address.y}
+                                    data-lng={address.x}
+                                    onClick={this.setMap}>
+                                    {address.jibunAddress}
+                                </AddressListItem>
+                            )
                         })}
-                        <li>
-
-                        </li>
-                    </ul>
-                </section>
+                    </AddressList>
+                </AddressSection>
                 <MapContainer id={'map'}></MapContainer>
-            </React.Fragment>
+            </Container>
         )
     }
 }
