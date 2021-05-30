2dff664f9ff3f22020b05243a8d057e50cce08e0
LeeByeongMuk <mansogod@gmail.com>
2021-05-31 01:09:13 +0900

    Update Header

--- src/components/UI/organisms/Header/index.js
+++ src/components/UI/organisms/Header/index.js
@@ -1,27 +1,19 @@
 import React, {memo} from "react";
 import styled from "styled-components";
-import {PageColor, White} from "../styles/variable";
+import Heading from "../../atoms/Heading";
 
 const HeaderWrapper = styled.header`
     display: flex;
     align-items: center;
     height: 60px;
     padding: 0 25px;
-    background: ${PageColor};
-`;
-
-const Title = styled.h1`
-    font-weight: bold;
-    font-size: 24px;
-    color: ${White}
 `;
 
 const Header = memo(() => {
     return (
         <HeaderWrapper>
-            <Title>Today Menu</Title>
+            <Heading level={1}
+                     children={"Today Menu"} />
         </HeaderWrapper>
     );
 });
-
-export default Header;
