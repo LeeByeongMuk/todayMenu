7065ef4d8b4680b6c2fff5acc26d2ab0482eee9e
LeeByeongMuk <mansogod@gmail.com>
2021-05-31 21:30:00 +0900

    Add Atoms Input component

---
+++ src/components/UI/atoms/Input/index.js
@@ -0,0 +1,20 @@
+import React, {memo} from "react";
+import styled from 'styled-components';
+import {FontColor} from "../../../../styles/variable";
+
+const styledInput = styled.input`
+    padding-left: 10px;
+    border: 0;
+    font-size: 16px;
+    color: ${FontColor};
+    box-sizing: border-box;
+    outline: none;
+`;
+
+const Input = memo(({...props}) => {
+    return (
+        <styledInput {...props} />
+    );
+});
+
+export default Input;
